577867f875564dd4a9b1eb1a5f724756dd91cb25
Aveev Bair <aveevb123@gmail.com>
2023-03-02 20:56:16 +0300

    feat: completed binary tree

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-// import BinaryRootComponent from './Components/BinaryRootComponent';
 import {useEffect, useState, useRef} from "react";
 
 const getNum = () => {
@@ -8,78 +7,78 @@ const getNum = () => {
    return num * sign;
 }
 
+class Node {
+   constructor(num) {
+      this.value = num;
+      this.left = null;
+      this.right = null;
+      this.positionY = null;
+      this.positionX = null;
+      this.key = null;
+   }
+}
+
+class BinaryTree {
+   constructor() {
+      this.root = null;
+   }
+   addNode(num) {
+      const newNode = new Node(num);
+      if (!this.root) {
+         this.root = newNode;
+         this.root.positionY = 10;
+         this.root.positionX = document.documentElement.clientWidth / 2;
+         this.key = +new Date();
+         return
+      }
+      let currentNode = this.root;
+      while (currentNode) {
+         if (newNode.value < currentNode.value) {
+            if (!currentNode.left) {
+               currentNode.left = newNode;
+               currentNode.left.positionY = currentNode.positionY + 50;
+               currentNode.left.positionX = currentNode.positionX - 50;
+               currentNode.left.key = +new Date();
+               return
+            }
+            currentNode = currentNode.left;
+         } else {
+            if (!currentNode.right) {
+               currentNode.right = newNode;
+               currentNode.right.positionY = currentNode.positionY + 50;
+               currentNode.right.positionX = currentNode.positionX + 50;
+               currentNode.right.key = +new Date();
+               return
+            }
+            currentNode = currentNode.right;
+         }
+      }
+   }
+}
+
 function App() {
    const [binaryTreeArr, setBinaryTreeArr] = useState([]);
-
-   class Node {
-      constructor(num) {
-         this.value = num;
-         this.left = null;
-         this.right = null;
-         this.positionY = null;
-         this.positionX = null;
-         this.key = null;
-      }
-   }
-
-   class BinaryTree {
-       constructor() {
-         this.root = null;
-      }
-      addNode(num) {
-         const newNode = new Node(num);
-         if (!this.root) {
-            this.root = newNode;
-            this.root.positionY = 10;
-            this.root.positionX = document.documentElement.clientWidth / 2;
-            setBinaryTreeArr([{
-               value : this.root.value,
-               positionY: this.root.positionY + 'px',
-               positionX: this.root.positionX + 'px',
-               key : +new Date(),
-            }]);
-            console.log('new root')
-            console.log(binaryTreeArr);
-            return
-         }
-         let currentNode = this.root;
-         while (currentNode) {
-            if (newNode.value < currentNode.value) {
-               if (!currentNode.left) {
-                  currentNode.left = newNode;
-                  currentNode.left.positionY = currentNode.positionY + 20;
-                  currentNode.left.positionX = currentNode.positionX - 20;
-                  setBinaryTreeArr([...binaryTreeArr, {
-                     value : currentNode.left.value,
-                     positionY: currentNode.left.positionY + 'px',
-                     positionX: currentNode.left.positionX + 'px',
-                     key : +new Date(),
-                  }]);
-                  console.log(binaryTreeArr);
-                  return
-               }
-            } else {
-               if (!currentNode.right) {
-                  currentNode.right = newNode;
-                  currentNode.right.positionY = currentNode.positionY + 20;
-                  currentNode.right.positionX = currentNode.positionX + 20;
-                  setBinaryTreeArr([...binaryTreeArr, {
-                     value : currentNode.right.value,
-                     positionY: currentNode.right.positionY + 'px',
-                     positionX: currentNode.right.positionX + 'px',
-                     key : +new Date(),
-                  }]);
-                  return
-               }
-               currentNode = currentNode.right;
-            }
-         }
-      }
-   }
    const binaryTree = useRef(new BinaryTree());
+   const sortBinaryTree = (tree) => {
+      const queue = [tree.root];
+      const renderArr = [];
+      while (queue.length > 0) {
+         const node = queue.shift();
+         renderArr.push(node);
+         if (node.left) {
+            queue.push(node.left);
+         }
+         if (node.right) {
+            queue.push(node.right);
+         }
+      }
+      setBinaryTreeArr(renderArr);
+   }
+
    const handlerSpaceClick = (e) => {
       if (e.code === 'Space') {
          binaryTree.current.addNode(getNum());
+         sortBinaryTree(binaryTree.current);
          console.log(binaryTreeArr);
       }
    };
